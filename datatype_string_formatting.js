088fd4c6fbba018dffe4e1520e2128108167eda5
Peter Lin <peterjunlin@gmail.com>
2021-02-26 23:44:06 -0800

    console, string formatting

---
+++ datatype_string_formatting.js
@@ -0,0 +1,4 @@
+// Use back tick to inline variable
+var my_name = "John";
+var s = `My name is ${my_name}.`
+console.assert(s == "My name is John.");
---
+++ global_console.js
@@ -0,0 +1,14 @@
+// assert if false
+console.assert(3 == 4, 'Not equal.');
+
+// Output to terminal, with format.
+console.log("The amount is %s", 123.456);  // %s Element is converted to a string
+
+// %d or %i 	Element is converted to an integer
+// %f 	Element is converted to a float
+// %o 	Element is displayed with optimally useful formatting
+// %O 	Element is displayed with generic JavaScript object formatting
+// %c 	Applies provided CSS
+
+// List all properties of an object
+console.dir(console);
